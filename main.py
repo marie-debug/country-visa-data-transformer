f1ca8b4bcd05e2f21d697ca976f343ae6570bd56
marie-debug <marionakinyi.ma@gmail.com>
2022-03-01 16:58:56 +1000

    changed countryrequirements set into pandas dataframe created new visas table stored visadataframe in postgres local db

--- main.py
+++ main.py
@@ -14,3 +14,26 @@ ct = CountryTable(data_url)
 countryList = ct.GetCountryList()
 
 print(countryList)
+
+countryRequirementSet = set()
+for countryRequirement in countryRequirementsResultList:
+    # print(countryRequirement)
+    countryRequirementSet.add(countryRequirement["Visa"].lower())
+
+#print(countryRequirementSet)
+
+engine = create_engine(data_url)
+
+
+def visadataframe(set):
+    visaDicList = []
+    for index, visas in enumerate(set):
+        visasdic = {'name': visas, 'id': index + 1}
+        visaDicList.append(visasdic)
+        #print(visaDicList)
+    return pd.DataFrame(visaDicList)
+
+
+visa=visadataframe(countryRequirementSet)
+
+visa.to_sql('visas', engine, if_exists='replace', index=False)
